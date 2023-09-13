91207c546ee211af39737fbe40d30c8d9bb05286
Simone Bria <s.bria@sagitek.com>
2023-09-13 17:46:46 +0200

    Initial commit

---
+++ src/main/java/com/twentyfive/twentyfivecontroller/TwentyfiveControllerApplication.java
@@ -0,0 +1,13 @@
+package com.twentyfive.twentyfivecontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwentyfiveControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwentyfiveControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twentyfive/twentyfivecontroller/TwentyfiveControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twentyfive.twentyfivecontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwentyfiveControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
